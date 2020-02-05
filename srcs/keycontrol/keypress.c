8b8ab281dba578d794d9bb244195d8d2454204f9
Dylan Gascon <dgascon@z1r3p4.le-101.fr>
2020-02-05 17:42:28 +0100

    Fct destroy delete, move in garbage.c

--- srcs/keycontrol/keypress.c
+++ srcs/keycontrol/keypress.c
@@ -6,23 +6,12 @@
 /*   By: dgascon <dgascon@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2020/01/31 05:10:46 by dgascon      #+#   ##    ##    #+#       */
-/*   Updated: 2020/02/04 19:46:46 by dgascon     ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/02/05 17:18:01 by dgascon     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-void destroy(t_data *data)
-{
-	mlx_destroy_image(data->mlx.ptr, data->Ftex.img);
-	mlx_destroy_image(data->mlx.ptr, data->Vtex.img);
-	mlx_destroy_image(data->mlx.ptr, data->Wtex.img);
-	mlx_destroy_image(data->mlx.ptr, data->Rtex.img);
-	mlx_destroy_image(data->mlx.ptr, data->image.img); //TODO destroy toute les textures
-	mlx_destroy_window(data->mlx.ptr, data->mlx.win);
-	exit(1);
-}
 
 int    key_press(int key, t_data *data)
 {
