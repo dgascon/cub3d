977cf3cc5109a56c9443b937191eb24e2d4c5157
Dylan Gascon <dgascon@z1r3p4.le-101.fr>
2020-02-05 20:37:11 +0100

    add fct, define

--- includes/player.h
+++ includes/player.h
@@ -6,7 +6,7 @@
 /*   By: dgascon <dgascon@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2020/01/09 17:27:41 by dgascon      #+#   ##    ##    #+#       */
-/*   Updated: 2020/02/05 16:47:36 by dgascon     ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/02/05 19:09:34 by dgascon     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -20,6 +20,7 @@
 # define POV_EAST 2 * M_PI
 # define POV_SOUTH 3 * M_PI_2
 # define POV_WEST M_PI
+# define MAX_SPEED 20
 
 typedef struct s_data t_data;
 
@@ -30,7 +31,7 @@ typedef struct	s_player
 	int		height_cam;
 	t_coord	pos;
 	int		speed;
-	float dist_proj_plane;
+	float 	dist_proj_plane;
 	int		CST;
 	int		hdv;
 	enum e_boolean show_minimap;
@@ -38,6 +39,6 @@ typedef struct	s_player
 
 int				key_press(int key, t_data *data);
 int				key_release(int key, t_data *data);
-int			destroy(t_data *data);
-
+int				destroy(t_data *data);
+void			move(t_data *data, float direction);
 #endif
