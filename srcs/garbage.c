2808a8aeaeac4a2ed975438cb5adf835dab91718
Dylan Gascon <dgascon@z1r3p4.le-101.fr>
2020-02-05 17:41:08 +0100

    Move fcnt destroy here

--- srcs/garbage.c
+++ srcs/garbage.c
@@ -3,15 +3,26 @@
 /*                                                              /             */
 /*   garbage.c                                        .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: dgascon <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*   By: dgascon <dgascon@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2020/01/31 04:37:46 by dgascon      #+#   ##    ##    #+#       */
-/*   Updated: 2020/01/31 05:09:05 by dgascon     ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/02/05 17:18:07 by dgascon     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+int destroy(t_data *data)
+{
+	mlx_destroy_image(data->mlx.ptr, data->Ftex.img);
+	mlx_destroy_image(data->mlx.ptr, data->Vtex.img);
+	mlx_destroy_image(data->mlx.ptr, data->Wtex.img);
+	mlx_destroy_image(data->mlx.ptr, data->Rtex.img);
+	mlx_destroy_image(data->mlx.ptr, data->image.img); //TODO destroy toute les textures
+	mlx_destroy_window(data->mlx.ptr, data->mlx.win);
+	exit(1);
+}
 
 void freemap(t_data *data, int offset)
 {
