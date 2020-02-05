8942aced69527ddce59198331d1f55184dde8d9f
Dylan Gascon <dgascon@z1r3p4.le-101.fr>
2020-02-05 17:41:43 +0100

    Type fct destroy

--- includes/player.h
+++ includes/player.h
@@ -6,7 +6,7 @@
 /*   By: dgascon <dgascon@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2020/01/09 17:27:41 by dgascon      #+#   ##    ##    #+#       */
-/*   Updated: 2020/02/04 19:43:18 by dgascon     ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/02/05 16:47:36 by dgascon     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -38,6 +38,6 @@ typedef struct	s_player
 
 int				key_press(int key, t_data *data);
 int				key_release(int key, t_data *data);
-void			destroy(t_data *data);
+int			destroy(t_data *data);
 
 #endif
