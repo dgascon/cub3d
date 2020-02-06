7687fcb37a115f1e6cefa7496d33709f5e212851
Dylan Gascon <dgascon@z2r7p6.le-101.fr>
2020-02-06 23:27:44 +0100

    Fix bux

--- srcs/move.c
+++ srcs/move.c
@@ -7,7 +7,7 @@ int move_x(t_data *data, int value)
 	tmp = data->player.pos.x + value;
 	if (tmp < (data->world.size.x * BLOCK_SIZE)
 	&&	tmp > BLOCK_SIZE
-	&&	data->world.map[data->player.pos.y / 64][tmp / 64] != '1')
+	&&	data->world.map[data->player.pos.y / BLOCK_SIZE][(tmp / BLOCK_SIZE)] != '1')
 			data->player.pos.x = tmp;
 	return (0);
 }
@@ -18,13 +18,13 @@ int move_y(t_data *data, int value)
 	tmp = data->player.pos.y + value;
 	if (tmp < (data->world.size.y * BLOCK_SIZE)
 	&&	tmp > BLOCK_SIZE
-	&&	data->world.map[tmp / 64][data->player.pos.x / 64] != '1')
+	&&	data->world.map[tmp / BLOCK_SIZE][data->player.pos.x / BLOCK_SIZE] != '1')
 			data->player.pos.y = tmp;
 	return (0);
 }
 
 void	move(t_data *data, float direction)
 {
-	move_x(data, cosf(direction) * data->player.speed); 
-	move_y(data, sinf(direction) * -data->player.speed);
+	move_x(data, cosf(direction) * data->player.speed / 2); 
+	move_y(data, sinf(direction) * -data->player.speed / 2);
 }
