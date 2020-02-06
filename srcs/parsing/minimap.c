53d58f9673e49fa7803d9bfee541ce09b7b346a5
Dylan Gascon <dgascon@z2r7p6.le-101.fr>
2020-02-06 23:27:54 +0100

    Update

--- srcs/parsing/minimap.c
+++ srcs/parsing/minimap.c
@@ -6,37 +6,48 @@
 /*   By: dgascon <dgascon@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2020/02/04 19:37:25 by dgascon      #+#   ##    ##    #+#       */
-/*   Updated: 2020/02/04 20:05:19 by dgascon     ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/02/06 23:26:52 by dgascon     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	minimap(t_data *data, int width, int height)
+#define RATIO 0.5f
+
+void	minimap(t_data *data)
 {
 	t_coord pos;
-	t_coord	size;
-	t_coord offset;
+	t_coord	sizemap;
+	t_coord sizebox;
+	char current_block;
 
-	offset = (t_coord) {.x = 1, .y = 1};
-	pos = (t_coord) {.y = offset.y};
-	size = (t_coord) {width, height};
-
-	mlx_rect(data, (t_coord) {offset.x - 1, offset.y - 1}, (t_coord) {data->world.size.x * BLOCK_SIZE + (data->world.size.x + 1), (data->world.size.y) * BLOCK_SIZE + data->world.size.y + 1}, rgb_int(255, 255, 255));
-	while (pos.y < data->world.size.y * BLOCK_SIZE)
+	pos = (t_coord) {};
+	sizebox.x = ((float)64 * ((float)16 / data->world.size.x)) * RATIO;
+	sizebox.y = ((float)64 * ((float)8 / data->world.size.y)) * RATIO;
+	sizemap.x = (data->world.size.x * sizebox.x);
+	sizemap.y = (data->world.size.y * sizebox.y);
+	if (!(data->minimap.img = mlx_new_image(data->mlx.ptr, sizemap.x, sizemap.y)))
+		printf("BITCH\n");
+	if (!(data->minimap.add_image = mlx_get_data_addr(data->minimap.img, &data->minimap.bpp, &data->minimap.size_line, &data->minimap.endian)))
+		printf("BITCH 2\n");
+	mlx_rect(&data->minimap, (t_coord) {}, sizemap, rgb_int(255, 255, 255));
+	while (pos.y < sizemap.y)
 	{
-		pos.x = offset.x;
-		while (pos.x < data->world.size.x * BLOCK_SIZE)
+		pos.x = 0;
+		while (pos.x < sizemap.x)
 		{
-			if (data->world.map[pos.y / BLOCK_SIZE][pos.x / BLOCK_SIZE] == '1')
-				mlx_rect(data, pos, size, rgb_int(100, 100, 100));
+			current_block = data->world.map[pos.y / sizebox.y][pos.x / sizebox.x];
+			if (current_block == '1')
+				mlx_rect(&data->minimap, pos, sizebox, rgb_int(100, 100, 100));
+			else if (current_block == '2')
+				mlx_rect(&data->minimap, pos, sizebox, rgb_int(180, 100, 100));
 			else
-				mlx_rect(data, pos, size, rgb_int(135, 135, 135));
-			
-			mlx_rect(data, (t_coord) {data->player.pos.x, data->player.pos.y}, (t_coord) {5, 5}, rgb_int(255, 105, 180));	
-			pos.x += size.x + offset.x;
+				mlx_rect(&data->minimap, pos, sizebox, rgb_int(135, 135, 135));
+			pos.x += sizebox.x;
 		}
-		pos.y += size.y + offset.y;
+		pos.y += sizebox.y;
 	}
+	mlx_rect(&data->minimap, (t_coord) {data->player.pos.x / ((float)(data->world.size.x * 64) / (float)sizemap.x), data->player.pos.y / ((float)(data->world.size.y * 64) / (float)sizemap.y)}, (t_coord) {5, 5}, rgb_int(255, 105, 180));	
+	mlx_put_image_to_window(data->mlx.ptr, data->mlx.win, data->minimap.img, 10, 10);
 }
