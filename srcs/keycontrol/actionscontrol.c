dfe2370db42513867319fa119a831b2edb4cc2cd
Nathan Lecaille <nlecaill@z4r7p5.le-101.fr>
2020-02-11 13:46:40 +0100

    vitesse de saut modifie

--- srcs/keycontrol/actionscontrol.c
+++ srcs/keycontrol/actionscontrol.c
@@ -6,7 +6,7 @@
 /*   By: nlecaill <nlecaill@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2020/02/05 17:55:22 by dgascon      #+#   ##    ##    #+#       */
-/*   Updated: 2020/02/10 14:30:52 by nlecaill    ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/02/11 13:46:11 by nlecaill    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -42,12 +42,12 @@ static void	releasedelay_heightcam(t_data *data, int *state_height_cam)
 {
 	if (*state_height_cam == 1)
 	{
-		data->player.height_cam += BLOCK_SIZE/10;
+		data->player.height_cam += BLOCK_SIZE/20;
 		(data->player.height_cam > 3*BLOCK_SIZE/4) ? *state_height_cam = 2 : 0;
 	}
 	else if (*state_height_cam == 2)
 	{
-		data->player.height_cam -= BLOCK_SIZE/10;
+		data->player.height_cam -= BLOCK_SIZE/20;
 		(data->player.height_cam == BLOCK_SIZE/2) ? *state_height_cam = 0 : 0;
 	}
 	if (!data->actions.crouching && *state_height_cam == 0)
