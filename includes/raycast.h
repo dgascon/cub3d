98d840224830bc2ea9f562e6e215031e59ea9eae
Dylan Gascon <dgascon@z2r7p1.le-101.fr>
2020-02-18 10:43:11 +0100

    header

--- includes/raycast.h
+++ includes/raycast.h
@@ -6,7 +6,7 @@
 /*   By: dgascon <dgascon@student.le-101.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/13 11:52:37 by dgascon           #+#    #+#             */
-/*   Updated: 2020/02/17 16:49:42 by dgascon          ###   ########lyon.fr   */
+/*   Updated: 2020/02/18 10:42:45 by dgascon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,6 @@ int				fill_column(t_data *data, int direction);
 float			linear_intersec_h(t_data *data);
 float			linear_intersec_v(t_data *data);
 void			fill_background(t_data *data);
+float			browse_h(t_data *data, t_f_coord intersec, t_f_coord offset,
+					t_coord grid);
 #endif
